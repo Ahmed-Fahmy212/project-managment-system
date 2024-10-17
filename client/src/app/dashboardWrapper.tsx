f80bcdc8b3c3402bd164d692ac278e5c697be6a6
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-10-17 21:14:31 +0300

    feat(Dashboard): add Sidebar component to dashboardWrapper

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Navbar from "./(components)/Navbar";
+import Sidebar from "./(components)/Sidebar";
 
 const dashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
       {/* sidebar */}
-      sidebar
+      <Sidebar />
       <main className="dark:bg-dark-bg w-full flex flex-col bg-gray-50 md:pl-64">
         <Navbar />
         {children}
