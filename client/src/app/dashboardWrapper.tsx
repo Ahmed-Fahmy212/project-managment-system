59407b69838498583f813d784c8d524f92be994f
ahmedfahmy <Ahmed-Fahmy212>
2024-10-18 20:19:38 +0300

    feat(Sidebar): update Sidebar component with logo and team information

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,17 +1,29 @@
+'use client'
+
 import React from "react";
 import Navbar from "./(components)/Navbar";
 import Sidebar from "./(components)/Sidebar";
+import StoreProvider from "./redux";
 
-const dashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  
   return (
-    <div className="flex min-h-screen w-full bg-gray-50 text-gray-900">
+    <div className="flex h-full min-h-screen w-full bg-gray-50 text-gray-900">
       {/* sidebar */}
       <Sidebar />
-      <main className="dark:bg-dark-bg w-full flex flex-col bg-gray-50 md:pl-64">
+      <main className="flex w-full flex-col bg-gray-50 dark:bg-dark-bg">
         <Navbar />
         {children}
       </main>
     </div>
+  );
+};
+
+const dashboardWrapper = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <StoreProvider>
+      <DashboardLayout> children</DashboardLayout>
+    </StoreProvider>
   );
 };
 
