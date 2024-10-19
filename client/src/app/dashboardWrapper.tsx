ff87ff914f94e35d4c38aaa64a6b0d26e676433b
ahmedfahmy <Ahmed-Fahmy212>
2024-10-19 17:31:13 +0300

    refactor(Dashboard): Update DashboardLayout component

--- client/src/app/dashboardWrapper.tsx
+++ client/src/app/dashboardWrapper.tsx
@@ -1,28 +1,39 @@
-'use client'
+"use client";
 
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./(components)/Navbar";
 import Sidebar from "./(components)/Sidebar";
-import StoreProvider from "./redux";
-
+import StoreProvider, { useAppSelector } from "./redux";
+/////////////////////////////////////////////////////////////////////////////
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
-  
+  const isDarkMode = useAppSelector((state) => state.global.isDarkMode);
+
+  useEffect(() => {
+    if (isDarkMode) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  });
+
+  //-----------------------------------------------------------------------------
   return (
     <div className="flex h-full min-h-screen w-full bg-gray-50 text-gray-900">
       {/* sidebar */}
       <Sidebar />
-      <main className="flex w-full flex-col bg-gray-50 dark:bg-dark-bg">
+      <main className={`flex w-full flex-col bg-gray-50 dark:bg-dark-bg`}>
         <Navbar />
         {children}
       </main>
     </div>
   );
 };
+/////////////////////////////////////////////////////////////////////////////
 
 const dashboardWrapper = ({ children }: { children: React.ReactNode }) => {
   return (
     <StoreProvider>
-      <DashboardLayout> children</DashboardLayout>
+      <DashboardLayout> {children}</DashboardLayout>
     </StoreProvider>
   );
 };
