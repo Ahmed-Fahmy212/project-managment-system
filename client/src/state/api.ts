148f8f1c8ef59700e4a6e3acd9d28a12f9e99f89
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-10-24 20:41:03 +0300

    refactor(Client): Update api.ts with additional interfaces for Project, User, Attachment, Task, SearchResults, and Team

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -1,11 +1,69 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-export const api = createApi({
-    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
-    reducerPath: "api",
-    tagTypes: [],
+export interface Project {
+    id: number;
+    name: string;
+    description?: string;
+    startDate?: string;
+    endDate?: string;
+}
+export enum Priority {
+    Urgant = "Urgant",
+    High = "High",
+    Medium = "Medium",
+    Low = "Low",
+    BackLog = "BackLog"
+}
 
-    endpoints: (builder) => ({
-    }),
-})
+export enum Status {
+    ToDo = "To Do",
+    WorkInProgress = "Work In Progress",
+    UnderReview = "Under Review",
+    Completed = "Completed",
+}
 
-export const { } = api
+export interface User {
+    id: number;
+    name: string;
+    email: string;
+    role: string;
+    profilePicture?: string;
+    cognitoid?: string;
+    teamId?: number;
+}
+export interface Attachment {
+    id: number;
+    fileURL: string;
+    fileName: string;
+    taskId: number;
+    uploadedById: number;
+}
+export interface Task {
+    id: number;
+    title: string;
+    description?: string;
+    status?: Status;
+    priority?: Priority;
+    tags?: string;
+    startDate?: string;
+    dueDate?: string;
+    points?: number;
+    projectId: number;
+    authorUserId?: number;
+    assignedUserId?: number;
+
+    author?: User;
+    assignee?: User;
+    comments?: Comment[];
+    attachments?: Attachment[];
+}
+export interface SearchResults {
+    tasks?: Task[];
+    projects?: Project[];
+    users?: User[];
+}
+export interface Team {
+    teamId: number;
+    teamName: string;
+    productOwnerUserId?: number;
+    projectManagerUserId?: number;
+}
