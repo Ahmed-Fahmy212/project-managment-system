d727bced1911957f6399966aa964357f233a9579
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-11-03 22:18:47 +0200

    fix(sidebar): fix small screen in sidebar

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
     extend: {
       colors: {
         white: "#ffffff",
-        gray:{
+        gray: {
           100: "#f3f4f6",
           200: "#e5e7eb",
           300: "#d1d5db",
@@ -19,14 +19,26 @@ const config: Config = {
           700: "#374151",
           800: "#1f2937",
         },
-        "dark-bg":'#101214',
-        "dark-secondary":'#1d1f21',
-        "dark-tertiary":'#3b3d40',
-        "blue-primary":'#0275ff',
+        "dark-bg": '#101214',
+        "dark-secondary": '#1d1f21',
+        "dark-tertiary": '#3b3d40',
+        "blue-primary": '#0275ff',
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
     },
+    screens: {
+      'smallScreen': '470px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px'
+    },
+    gridTemplateColumns: {
+      '16': 'repeat(16, minmax(0, 1fr))',
+      'footer': 'repeat(4, 260px)',
+    }
   },
   plugins: [],
 };
