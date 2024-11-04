51e130ce983a781ead6876fe4db29b66ed1defaa
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-11-04 15:07:22 +0200

    feat: Add react-hot-toast for displaying error messages

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import DashboardWrapper from "./dashboardWrapper";
+import { ToastProvider } from "@/components/toaster-provider";
 const inter = Inter({subsets : ["latin"]});
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body
         className={`${inter.className}`}
       >
+        <ToastProvider />
         <DashboardWrapper>{children}</DashboardWrapper>
       </body>
     </html>
