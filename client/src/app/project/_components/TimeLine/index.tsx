aaca6e9ad06e70f19723b1f405d02bb555bb05aa
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-11-09 20:17:17 +0200

    refactor(Timeline): improve Timeline component UI and add loading and error handling

--- client/src/app/project/_components/TimeLine/index.tsx
+++ client/src/app/project/_components/TimeLine/index.tsx
@@ -54,41 +54,41 @@ export const Timeline = ({ id, setIsModalNewTaskOpen }: Props) => {
     return (
         <div className="px-4 xl:px-6">
             {isLoading ? (
-            <div>Loading...</div>
+                <div>Loading...</div>
             ) : error ? (
-            <div>Error...</div>
+                <div>Error...</div>
             ) : (
-            <div className="px-4 xl:px-6">
-                <div className="flex py-5">
-                <Header
-                    name="Timeline"
-                    isSmallText
-                    buttonComponent={
-                    <button
-                        className="flex items-center rounded text-nowrap bg-gray-600 px-3 py-2 text-white cursor-not-allowed"
-                        onClick={() => setIsModalNewTaskOpen(true)}
-                        disabled
-                    >
-                        Add New Task
-                    </button>
-                    }
-                />
-                <div className="w-64 pl-4">
-                    <select
-                    className="focus:shadow-outline block w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 pr-8 leading-tight shadow hover:border-gray-500 focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white"
-                    value={displayOptions.viewMode}
-                    onChange={handleViewModeChange}
-                    >
-                    <option value={ViewMode.Day}>Day</option>
-                    <option value={ViewMode.Week}>Week</option>
-                    <option value={ViewMode.Month}>Month</option>
-                    </select>
+                <div className="px-4 xl:px-6">
+                    <div className="flex py-5">
+                        <Header
+                            name="Timeline"
+                            isSmallText
+                            buttonComponent={
+                                <button
+                                    className="flex items-center rounded text-nowrap bg-gray-600 px-3 py-2 text-white cursor-not-allowed"
+                                    onClick={() => setIsModalNewTaskOpen(true)}
+                                    disabled
+                                >
+                                    Add New Task
+                                </button>
+                            }
+                        />
+                        <div className="w-64 pl-4">
+                            <select
+                                className="focus:shadow-outline block w-full appearance-none rounded border border-gray-400 bg-white px-4 py-2 pr-8 leading-tight shadow hover:border-gray-500 focus:outline-none dark:border-dark-secondary dark:bg-dark-secondary dark:text-white"
+                                value={displayOptions.viewMode}
+                                onChange={handleViewModeChange}
+                            >
+                                <option value={ViewMode.Day}>Day</option>
+                                <option value={ViewMode.Week}>Week</option>
+                                <option value={ViewMode.Month}>Month</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div className="flex justify-center items-center h-full dark:text-white">
+                        Coming Soon ...
+                    </div>
                 </div>
-                </div>
-                <div className="flex justify-center items-center h-full">
-                Coming Soon ...
-                </div>
-            </div>
             )}
         </div>
     );
