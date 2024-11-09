b93d2470604b8bbdb004000387c0b3b3bd5af632
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-11-09 20:08:07 +0200

    feat(errorMiddleware): add enhanced error handling for Prisma and Zod validation

--- server/src/middleware/errorHandlerMiddleware.ts
+++ server/src/middleware/errorHandlerMiddleware.ts
@@ -1,7 +1,46 @@
 import { Request, Response, NextFunction } from 'express';
-
+import { Prisma } from '@prisma/client';
+import { ZodError } from 'zod';
 import { HttpException } from '../exceptions/HttpExceptions';
 const errorHandlerMiddleware = (err: HttpException, req: Request, res: Response, next: NextFunction) => {
+    
+    if (err instanceof Prisma.PrismaClientKnownRequestError) {
+        let message = 'A database error occurred';
+        let statusCode = 500;
+
+        if (err.code === 'P2002') {
+            message = 'Unique constraint failed on one or more fields.';
+            statusCode = 400;
+        } else if (err.code === 'P2025') {
+            message = 'Record not found.';
+            statusCode = 404;
+        }else if (err.code === 'P2003') {
+            message = 'Invalid input data:';
+            statusCode = 404;
+        }
+        else if (err.code === 'P2014') {
+            message = `Invalid ID: ${err.message}`;
+            statusCode = 404;
+        }
+
+        res.status(statusCode).json({
+            error: {
+                message,
+            },
+        });
+    }
+    if (err instanceof ZodError) {
+        const errorDetails = err.errors.map((e) => ({
+            item: e.path.join('.'),
+            message: e.message,
+        }));
+        res.status(400).json({
+            error: {
+            message: 'Bad Request',
+            details: errorDetails,
+            },
+        });
+    }
     const error = err instanceof HttpException ? err : new HttpException('Internal Server Error', 500);
 
     res.status(error.statusCode || 500).json({
@@ -11,6 +50,6 @@ const errorHandlerMiddleware = (err: HttpException, req: Request, res: Response,
             stack: process.env.NODE_ENV === 'development' ? error.stack : undefined,
         },
     });
-    
+    next();
 };
 export default errorHandlerMiddleware;
