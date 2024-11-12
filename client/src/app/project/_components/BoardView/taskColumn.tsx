29c77abc84fa6d113f8e84a7a917057d71b41b3e
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-11-12 22:54:46 +0200

    Refactor taskColumn component to add dropdown menu for column actions

--- client/src/app/project/_components/BoardView/taskColumn.tsx
+++ client/src/app/project/_components/BoardView/taskColumn.tsx
@@ -1,27 +1,57 @@
-import { Status, useGetTasksQuery, useUpdateTaskMutation } from "@/state/api";
-import { Task as Tasks } from "@/state/api";
-import { format } from "date-fns";
-import Image from "next/image";
-import { EllipsisVertical, MessageSquareMore, Plus } from "lucide-react";
+'use client';
+import {
+    Cloud,
+    Github,
+    Keyboard,
+    LifeBuoy,
+    LogOut,
+    Mail,
+    MessageSquare,
+    PlusCircle,
+    Settings,
+    User,
+    UserPlus,
+    Users,
+} from "lucide-react"
+import { Task as TaskComponent } from './task'
+import { CreditCard, EllipsisVertical, MessageSquareMore, Plus } from "lucide-react";
 import toast from "react-hot-toast";
 import { DndContext, DragEndEvent, useDroppable } from '@dnd-kit/core'
-import { Task } from "./task";
-
+import { useState } from "react";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { DropdownMenuShortcut } from "@/components/ui/dropdown-menu";
+import { QueryClient, useMutation, useQueryClient } from "@tanstack/react-query";
+import { addColumn } from "@/state/column,api";
+import ColumnForm from "./columnForm";
 
 type TaskColumnProps = {
+    id: number;
     status: string;
-    tasks: Tasks[];
+    tasks: any[];
     setIsModalNewTaskOpen: (isopen: boolean) => void;
+    addColumnMutation: (column: { title: string; color: string; projectId: number }) => Promise<void>;
 };
 export const TaskColumn = ({
+    id,
     status,
     tasks,
     setIsModalNewTaskOpen,
+    addColumnMutation
 }: TaskColumnProps) => {
-
-    const { setNodeRef, isOver} = useDroppable({
+    // id is not used
+    const { setNodeRef, isOver } = useDroppable({
         id: status
     })
+
+    const [open, setOpen] = useState(false);
+
+
+    // const tasksMutation = useMutation({
+    //     mutationFn: (newTask) => {
+    //         return axios.post(`http://localhost:8000/tasks`, newTask)
+    //     },
+    // })
     // const [{ isOver }, drop] = useDrop(() => ({
     //     accept: "task",
     //     drop: (item: { id: number }) => moveTask(item.id, status),
@@ -29,6 +59,7 @@ export const TaskColumn = ({
     //         isOver: !!monitor.isOver(),
     //     }),
     // }));
+    // remove this 
     const taskCount = tasks.filter((item) => item.status === status).length;
 
     const statusColor: any = {
@@ -41,8 +72,8 @@ export const TaskColumn = ({
     return (
         <div
             ref={setNodeRef}
-            className={`rounded-lg py-2 h-full  sm:py-4 xl:px-2 ${isOver ?
-                "bg-blue-200 dark:bg-neutral-950 transition duration-500" : ""}`}
+            className={`rounded py-2 h-full  sm:py-4 xl:px-2 ${isOver ?
+                "bg-blue-200 dark:bg-black transition duration-500" : ""}`}
         >
             <div className="flex mb-3 w-full">
                 <div
@@ -60,9 +91,40 @@ export const TaskColumn = ({
                         </span>
                     </h3>
                     <div className="flex gap-1 items-center">
-                        <button className="dark:text-neutral-500 flex h-6 items-center justify-center w-5">
-                            <EllipsisVertical size={26} />
-                        </button>
+                        <div>
+                            {/* //TODO this is bad */}
+                            <DropdownMenu open={open} onOpenChange={setOpen}>
+                                <DropdownMenuTrigger asChild>
+                                    <button className="dark:text-neutral-500 flex h-6 items-center justify-center w-5"
+                                        onClick={() => setOpen(!open)}>
+                                        <EllipsisVertical />
+                                    </button>
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent className=" bg-white p-4 flex flex-col gap-2 justify-center items-center">
+                                    <DropdownMenuItem>
+                                        {/* <button className="hover:bg-gray-100 dark:hover:bg-gray-700 text-black dark:text-white"> */}
+                                        <div className="w-full h-full relative">
+
+                                        <ColumnForm projectId={Number(id)} AddColumnMutation={addColumnMutation} isSmallItem />
+                                        </div>
+                                        {/* </button> */}
+                                    </DropdownMenuItem>
+
+                                    <DropdownMenuItem>
+                                        <button className=" py-3 mr-2 h-4  ">Order</button>
+
+                                    </DropdownMenuItem>
+
+                                    <DropdownMenuItem>
+                                        <button className="pt-3   mr-2 h-4 "> Delete</button>
+
+                                    </DropdownMenuItem>
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+
+
+
+                        </div>
                         <button
                             className="bg-gray-200 dark:bg-dark-tertiary dark:text-white flex h-6 items-center justify-center rounded w-6"
                             onClick={() => setIsModalNewTaskOpen(true)}
@@ -73,13 +135,16 @@ export const TaskColumn = ({
                 </div>
             </div>
             {/*  O(2n) = O(n) ------------------------- O(nlog(n))*/}
-            {tasks
-                //TODO i think this bad
-                .filter((task) => task.status === status) // [{},{},{}] =>  
-                .sort((a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime())
-                .map((task) => (
-                    <Task task={task} />
-                ))}
-        </div>
+            {
+                tasks
+                    //todo Remove and make it single endpoint according to update single element or find a way insdie this nested column->tasks
+                    .filter((task) => task.status === status) // [{},{},{}] =>  
+                    // .sort((a, b) => new Date(b.updatedAt).getTime() - new Date(a.updatedAt).getTime())
+                    .map((task) => (
+                        <TaskComponent task={task} />
+                    ))
+            }
+
+        </div >
     );
 };
