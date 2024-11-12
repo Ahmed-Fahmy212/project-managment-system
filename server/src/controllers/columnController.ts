dcd76cf4ce177e35e89dcb27d998e52eb5da29e9
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-11-12 22:46:34 +0200

    feat columnController add some endpoints

--- server/src/controllers/columnController.ts
+++ server/src/controllers/columnController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { BadRequestException } from '../exceptions/BadRequestException';
 import response from '../util/responce';
 import { ColumnService } from '../services/columnService';
+import { ColumnDataSchema, UpdatedColumnData } from '../types/column.zod';
 
 
 export const columns = {
@@ -16,5 +17,54 @@ export const columns = {
         }
         const columns = await ColumnService.getColumnWithTasks(parsedProjectId);
         response.success(res, columns);
+    },
+    getColumns: async (
+        req: Request,
+        res: Response,
+    ) => {
+        const columns = await ColumnService.getAllColumns();
+        response.success(res, columns);
+    },
+
+    getOneColumn: async (
+        req: Request,
+        res: Response,
+    ) => {
+        const { id } = req.params;
+        if (!id) throw new BadRequestException("Missing required field: id");
+
+        const column = await ColumnService.getColumnById(parseInt(id));
+        if (!column) throw new BadRequestException("Column not found");
+
+        response.success(res, column);
+    },
+
+    createColumn: async (
+        req: Request,
+        res: Response,
+    ) => {
+        const validatedData = ColumnDataSchema.parse(req.body);
+        const createdColumn = await ColumnService.createColumn(validatedData);
+        response.success(res, createdColumn);
+    },
+
+    updateColumn: async (
+        req: Request,
+        res: Response,
+    ) => {
+        const validatedData = UpdatedColumnData.parse(req.body);
+        //TODO calc time and num req for this 
+        const updatedColumn = await ColumnService.updateColumn(validatedData);
+        response.success(res, updatedColumn);
+    },
+
+    deleteColumn: async (req: Request,
+        res: Response,) => {
+        const { id } = req.params;
+        if (!id) throw new BadRequestException("Missing required field: id");
+
+        await ColumnService.deleteColumn(parseInt(id));
+        response.success(res, { message: "Column deleted successfully" });
     }
+
 }
