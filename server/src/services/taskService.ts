c08a0886e54d1c98dcf8f12871eb673660c0a4a9
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-11-23 14:01:49 +0200

    update with lock

--- server/src/services/taskService.ts
+++ server/src/services/taskService.ts
@@ -58,49 +58,66 @@ export const TaskService = {
             throw new NotFoundException("Task didn`t created");
         }
         return newTask;
-        },
+    },
 
-        updateTaskStatus: async (
+    updateTaskStatus: async (
         body: zod.infer<typeof UpdatedTaskData>
-        ): Promise<{ newOrderedTasks: Task[] }> => {
+    ): Promise<{ newOrderedTasks: Task[] }> => {
         const { newOrder, projectId, columnId, activeTaskId: taskIdToMove } = body;
         if (taskIdToMove && !columnId || columnId && !taskIdToMove) {
-            throw new BadRequestException("Missing required field: columnId or activeTaskId");
+            throw new BadRequestException(`Missing required field: ${columnId ? "Task Id" : "Column Id"} `);
         }
         try {
             const fields = ["id", "order"];
             const taskValues = newOrder.map((task) => [task.id, task.order]);
-
+            // flages crash ?
+            let taskCounter = 0;
             let i = 0;
             const taskValuesSql = taskValues
-            .map((row) => `(${row.map(() => `\$${++i}`).join(", ")})`).join(", ");
+                .map((row) => `(${row.map(() => `\$${++i}`).join(", ")})`)
+                .join(", ");
+
             const otherTasksSql = `
+            WITH locked_tasks AS (
+            SELECT "id"
+            FROM "Task"
+            WHERE "projectId" = $${++i}
+            FOR UPDATE
+            )
             UPDATE "Task"
-            SET "order" = "t"."order" 
+            SET "order" = t."order"
             FROM (VALUES ${taskValuesSql}) AS t(${fields.map((f) => `"${f}"`).join(", ")})
-            WHERE "Task"."id" = "t"."id" AND "Task"."projectId" = $${++i}
+            WHERE "Task"."id" = t."id" AND "Task"."id" IN (SELECT "id" FROM locked_tasks)
             RETURNING "Task".*;
             `;
+            //!not working here + see this lock 
             let j = 0;
             const activeTaskSql = `
+            WITH locked_task AS (
+            SELECT "id"
+            FROM "Task"
+            WHERE "id" = $${++j} AND "projectId" = $${++j}
+            FOR UPDATE
+            )
             UPDATE "Task"
             SET "columnId" = $${++j}
-            WHERE "id" = $${++j} AND "projectId" = $${++j}
+            WHERE "id" IN (SELECT "id" FROM locked_task)
             RETURNING "Task".*;
             `;
 
+
             const updatedTasks = await prisma.$transaction([
-            prisma.$queryRawUnsafe(otherTasksSql, ...taskValues.flat(), projectId),
-            ...(taskIdToMove && columnId
-                ? [prisma.$queryRawUnsafe(activeTaskSql, columnId, taskIdToMove, projectId)]
-                : []),
+                prisma.$queryRawUnsafe(otherTasksSql, ...taskValues.flat(), projectId),
+                ...(taskIdToMove && columnId
+                    ? [prisma.$queryRawUnsafe(activeTaskSql, columnId, taskIdToMove, projectId)]
+                    : []),
             ]);
             const newOrderedTasks = (taskIdToMove && columnId) ? (updatedTasks[1] as Task[]).flat() : (updatedTasks as Task[]).flat();
             return { newOrderedTasks };
         } catch (error) {
             throw new Error("Failed to update task status");
         }
-        }
+    }
     // getUserTasks: async (
     //     req: Request,
     //     res: Response
