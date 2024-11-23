7a941fa74b856a4fcbd1aa34a2a745311b3f5c96
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-11-23 20:06:38 +0200

    add nodemailer config

---
+++ server/src/configs/nodemailer.config.ts
@@ -0,0 +1,12 @@
+
+export const config = {
+  email: {
+    host: "smtp.ethereal.email",
+    port: 587,
+    secure: false,
+    auth: {
+      user: "maddison53@ethereal.email",
+      pass: "jn7jnAPss4f63QBp6D",
+    },
+  },
+};
