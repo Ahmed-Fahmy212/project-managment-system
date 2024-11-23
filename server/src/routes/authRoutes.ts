41177d4061d91d8d3795d8ce4d08cbc56459738a
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-11-23 23:49:18 +0200

    initial auth routes

---
+++ server/src/routes/authRoutes.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import { AuthService } from '../services';
+
+const router = express.Router();
+
+
+// router.post('/login', AuthService.login);
+// router.post('/register', AuthService.register);
+// router.post('/refresh-token', AuthService.refreshToken);
+// router.post('/forgot-password', AuthService.forgotPassword);
+// router.post('/reset-password', AuthService.resetPassword);
