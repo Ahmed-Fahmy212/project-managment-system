a4b5fb8ce451661c10359d796baa51e0cd8c5dc5
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-11-24 20:42:03 +0200

    Remove AuthService from service index

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -2,7 +2,6 @@ export { ProjectService } from './project.service'
 export { TaskService } from './task.service'
 export { ColumnService } from './column.service'
 export { UserService } from './user.service'
-export { AuthService } from './auth.service'
 
 
 
