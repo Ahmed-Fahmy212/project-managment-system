e2a937bd22f587829da78815511d0f490049b182
Ahmed-Fahmy212 <126887643+Ahmed-Fahmy212@users.noreply.github.com>
2024-11-24 17:09:44 +0200

    Refactor all zod files name and folder structure

--- server/src/validations/auth.zod.ts
+++ server/src/validations/auth.zod.ts
@@ -21,7 +21,7 @@ const ResetPasswordSchema = z.object({
     email: z
         .string()
         .email("Invalid email address")
-    , password: z.string()
+    , newPassword: z.string()
         .min(6, "Password must be at least 6 characters long")
         .max(100, "Password must be at most 100 characters long")
     ,
