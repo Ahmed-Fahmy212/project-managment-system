024f01a961a3c273c473b9122306bdbd9f9174bf
ahmedfahmy <Ahmed-Fahmy212>
2024-12-14 11:25:26 +0200

    feat(Auth) add service .

--- server/src/services/index.ts
+++ server/src/services/index.ts
@@ -1,7 +1,8 @@
 export { ProjectService } from './project.service'
 export { TaskService } from './task.service'
 export { ColumnService } from './column.service'
-// export { UserService } from './user.service'
+export { UserService } from './user.service'
+export { authService } from './auth.service'
 
 
 
